0d2e9af2d20a61f2813a727ff0a89b0959f77e49
SAHAPUN-DEV <85986699+SAHAPUN-DEV@users.noreply.github.com>
2021-06-16 14:33:52 +0700

    Add files via upload

---
+++ Transactions.dart
@@ -0,0 +1,15 @@
+// ignore: unused_import
+import 'dart:ffi';
+
+class Transactions {
+  String title;
+  String province;
+  double amount;
+  DateTime date;
+
+  Transactions(
+      {required this.title,
+      required this.province,
+      required this.amount,
+      required this.date});
+}
